9ad1d0ac58a874beff3ee6887851fcd07f5f8ce8
ehab-tarek <ehabtarek144@gmail.com>
2025-02-18 15:45:03 +0200

    feat(logging): add morgan middleware for HTTP request logging

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,5 +1,5 @@
 import connectDb from "../src/database/connection.js";
-import { globalErrorHandling } from "./middleware/index.js";
+import { globalErrorHandling, morganMiddleware } from "./middleware/index.js";
 import authController from "./modules/auth/auth.controller.js";
 import userController from "./modules/user/user.controller.js";
 import postController from "./modules/post/post.controller.js";
@@ -9,31 +9,41 @@ import rateLimit from "express-rate-limit";
 import { AppError } from "./utils/appError.js";
 import { message } from "./common/constants/messages.constants.js";
 import helmet from "helmet";
+
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   handler: (req, res, next) => {
     return next(new AppError(message.common.tooManyRequest, 429));
   },
+  skip: (req) => req.ip === '127.0.0.1',
   legacyHeaders: true,
   standardHeaders: 'draft-8',
 });
+
 const bootstrap = (app, express) => {
+
+
+  app.use(morganMiddleware);
   app.use(cors());
   app.use(helmet());
   app.use('/post', limiter);
   app.use('/uploads', express.static(path.resolve('./src/uploads')));
   app.use(express.json());
-  app.get('/', (req, res) => res.send('Hello World!'))
+
+  app.get('/', (req, res) => res.send('Hello World!'));
 
   app.use('/auth', authController);
   app.use('/user', userController);
   app.use('/post', postController);
+
   app.all('*', (req, res) => {
     res.status(404).json({ message: 'Route not found' });
   });
 
   app.use(globalErrorHandling);
+
   connectDb();
 }
 
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,3 +2,4 @@ export * from './validation.middleware.js';
 export * from './globalErrorHandling.js';
 export * from './auth.middleware.js';
 export * from './validate-mongo-id.middleware.js';
+export * from './loggingMiddleware.js';
